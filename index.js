c71bc3d36ded11e94cdc86a0a190cf36e6c96828
Mark Korneychik <m.korneychik@awatera.com>
2020-03-19 03:54:39 +0300

    added error messages

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 const path = require("path");
 const readline = require('readline');
 
+const ALIVE_CHAR = "*";
+const DEAD_CHAR = ".";
+
 const generateCell = () => Math.random() > 0.5 ? 0 : 1;
 
 const generateLine = len => new Array(len).fill().map(generateCell);
@@ -36,7 +39,7 @@ const getNextIteration = field => {
 const frame = field => {
     console.clear();
     for (let i = 0; i < field.length; i++) {
-        console.log("".concat(...field[i].map(value => value ? "*" : ".")));
+        console.log("".concat(...field[i].map(value => value ? ALIVE_CHAR : DEAD_CHAR)));
     }   
 
     field = getNextIteration(field);
@@ -52,7 +55,21 @@ const init = () => {
         let rl = readline.createInterface(readStream);
 
         rl.on("line", line => { 
-            field.push(line.split("").map(c => c === "*" ? 1 : 0));
+            field.push(line.split("").map(c => {
+                switch(c) {
+                    case ALIVE_CHAR:
+                        return 1;
+                    case DEAD_CHAR:
+                        return 0;
+                    default:
+                        console.log("Error unknown charachter", c, "in file"); 
+                        process.exit(0);    
+                }
+            }));
+            if (field.length > 1 && field[0].length !== field[field.length - 1].length) {
+                console.log("Error lines have different length"); 
+                process.exit(0); 
+            }
         }).on("close", () => {
             frame(field);
         });
